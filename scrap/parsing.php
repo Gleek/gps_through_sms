9274f536ef42d8794774b3e4ae733a0dbef4cf10
Umar Ahmad <ahmad.umar2008@yahoo.in>
2013-07-18 23:19:00 +0530

    working on parser

--- scrap/parsing.php
+++ scrap/parsing.php
@@ -5,9 +5,14 @@ if (mysqli_connect_errno($con)){
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }*/
 
-
+require "simple_html_dom.php";
+$market=array("Lajpat-Nagar","Chandni-Chowk");
+$keyword=array("Cloth-shops","restaurant",)
+$html = file_get_html('http://www.justdial.com/Delhi-NCR/'.$keyword[0].'%3Cnear%3E-'.$market[0]);
+echo $html;
 
 //mysqli_close($con);
+
 
  ?>
 
