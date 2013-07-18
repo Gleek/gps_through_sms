6afac8123322650a4e33159ef1ac4dce05e58e85
Vipul Nayyar <nayyar_vipul@yahoo.com>
2013-07-19 02:43:35 +0530

    modified config

--- nav/kookoo/lib/config.inc.php
+++ nav/kookoo/lib/config.inc.php
@@ -11,7 +11,7 @@ $dbhandle = mysql_connect($hostname, $username, $password)
   or die("Unable to connect to MySQL");
 echo "Connected to MySQL<br>";
 
-$selected = mysql_select_db("peerhack",$dbhandle)
+$selected = mysql_select_db("thinkdif_peerhack",$dbhandle)
   or die("Could not select peerhack db");
 
 ?>
