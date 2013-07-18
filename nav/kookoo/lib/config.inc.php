2c13ef31ee608b1b4106807c0755a2181cb0a34d
Vipul Nayyar <nayyar_vipul@yahoo.com>
2013-07-19 01:37:13 +0530

    modified config

--- nav/kookoo/lib/config.inc.php
+++ nav/kookoo/lib/config.inc.php
@@ -31,44 +31,6 @@ if($_SERVER['SERVER_PORT'] == '80'  &&  SSL_ENABLE == true ) {
 
 //define tables
 
-define("TB_COUNTRY", "country");
-define("MYSQL_SERVER","engineerinme.com");
-define("MYSQL_USER","root");
-define("MYSQL_PASS","kgggdkp2692");
-define("MYSQL_DB","peerhack");
-define("RDBMS","MYSQL");
-
-
-
-
-define("TB_ADMINEM","tbl_adminemail");
-define("TB_ADMIN","tbl_admin");
-define("TB_PAGES","tbl_pages");
-define("TB_RIGHT","tbl_copyright");
-define("TB_IMGB","tbl_imgbaner");
-define("TB_MAIL","tbl_mail");
-define("TB_HOME","tbl_home");
-define("TB_PAGES","tbl_pages");
-
-
-
-
-define("WEBSITE_NAME","peerhack.herokuapp.com");
-
-
-require("database.inc");
-
-$dbh = new cDatabases();
-$dbh->Set(RDBMS);
-$dbh->Connect(MYSQL_SERVER,MYSQL_USER,MYSQL_PASS,MYSQL_DB);
-
-
-
-
-define("ENABLE_URL_REWRIING",true) ;         // check if URL Rewiting enable
-define("DEFAULT_POST_NAME","fld_id");            // this is default name of post/get name
-//include_once("url_rewrite.php");
-
 ?>
 
 
