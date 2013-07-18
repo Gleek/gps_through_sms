394e613e1cf118e1f566fd6a68fb0cfd60b31198
Vipul Nayyar <nayyar_vipul@yahoo.com>
2013-07-19 01:31:51 +0530

    modified config

--- nav/kookoo/lib/config.inc.php
+++ nav/kookoo/lib/config.inc.php
@@ -8,10 +8,10 @@
 #set_time_limit(0);
 error_reporting(E_ALL  & ~E_NOTICE & ~E_DEPRECATED);
 
-$con=mysqli_connect("engineerinme.com","root","kgggdkp2692","peerhack");
-if (mysqli_connect_errno())
+$con=mysql_connect("engineerinme.com","root","kgggdkp2692","peerhack");
+if (mysql_connect_errno())
   {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  echo "Failed to connect to MySQL: " . mysql_connect_error();
   die();
   }
 
@@ -20,8 +20,8 @@ if (mysqli_connect_errno())
 
 
 /*************************************************************************/
-//if ssl enable 
-//define ("SSL_ENABLE", false);   
+//if ssl enable
+//define ("SSL_ENABLE", false);
 /*
 if($_SERVER['SERVER_PORT'] == '80'  &&  SSL_ENABLE == true ) {
    header("location: https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
@@ -55,17 +55,17 @@ define("TB_PAGES","tbl_pages");
 
 define("WEBSITE_NAME","peerhack.herokuapp.com");
 
- 
+
 require("database.inc");
 
 $dbh = new cDatabases();
 $dbh->Set(RDBMS);
-$dbh->Connect(MYSQL_SERVER,MYSQL_USER,MYSQL_PASS,MYSQL_DB); 
+$dbh->Connect(MYSQL_SERVER,MYSQL_USER,MYSQL_PASS,MYSQL_DB);
 
 
 
 
-define("ENABLE_URL_REWRIING",true) ;         // check if URL Rewiting enable  
+define("ENABLE_URL_REWRIING",true) ;         // check if URL Rewiting enable
 define("DEFAULT_POST_NAME","fld_id");            // this is default name of post/get name
 //include_once("url_rewrite.php");
 
