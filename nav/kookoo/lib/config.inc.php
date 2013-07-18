c6ea6986cda283301920f5b1e7164977c1d6f3d8
=hammad haleem <hammadhaleem@gmail.com>
2013-07-18 16:11:32 -0400

    sdf

--- nav/kookoo/lib/config.inc.php
+++ nav/kookoo/lib/config.inc.php
@@ -1,36 +1,15 @@
 <?php
-#####################################################################
-#																	#
-#				Script : Configuraion file							#
-#																	#
-#				 													#
-#####################################################################
 #set_time_limit(0);
 error_reporting(E_ALL  & ~E_NOTICE & ~E_DEPRECATED);
 
-$con=mysql_connect("engineerinme.com","root","kgggdkp2692","peerhack");
-if (mysql_connect_errno())
+$con=mysqli_connect("engineerinme.com","root","kgggdkp2692","peerhack");
+if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: ";
   die();
   }
-
-
-// For Live
-
-
-/*************************************************************************/
-//if ssl enable
-//define ("SSL_ENABLE", false);
-/*
-if($_SERVER['SERVER_PORT'] == '80'  &&  SSL_ENABLE == true ) {
-   header("location: https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-}*/
-/**************************************************************************************************/
-
-
-//define tables
-
+else
+{
+echo "connected";
+}
 ?>
-
-
