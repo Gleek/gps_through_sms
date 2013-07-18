a57b9a63637fe41dff543f012a9af307a5db4b66
Vipul Nayyar <nayyar_vipul@yahoo.com>
2013-07-19 03:43:30 +0530

    modified config

--- nav/kookoo/msg.php
+++ nav/kookoo/msg.php
@@ -10,7 +10,7 @@ if(isset($_REQUEST['event']) && $_REQUEST['event']=="NewSms"){
 	$message=$_REQUEST['message'];
 	$time=$_REQUEST['time'];
 	$sql="INSERT INTO `inbound_msgs` (`message`, `time`, `flag`) VALUES ('".$message."','".$time."',1)";
-	echo $sql;
+	//echo $sql;
 	mysql_query($sql, $con);
 	//mysql_error();
 
