dd765f7a1253f72c3a1607eb9f56b8c76d2ad33e
Vipul Nayyar <nayyar_vipul@yahoo.com>
2013-07-19 01:28:39 +0530

    updated msg.php

--- nav/kookoo/msg.php
+++ nav/kookoo/msg.php
@@ -1,14 +1,20 @@
 <?php
 
-$myFile = "testFile.txt";
-$fh = fopen($myFile, 'w') or die("can't open file");
+include "lib/config.inc.php";
 
 
 if(isset($_REQUEST['event']) && $_REQUEST=="NewSMS"){
 
-	fwrite($fh, $_REQUEST['message']." from ".$_REQUEST['cid']);
+	$message=$_REQUEST['message'];
+	$time=$_REQUEST['time'];
+	mysqli_query($con,"INSERT INTO inbound_msgs (message, time, flag) VALUES ('$message','$time' ,1)");
+
+
 
 }
+
+mysqli_close($con);
+
 
 
 
