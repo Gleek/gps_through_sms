8f614798f7755acd69183f7e2cf8eaf449842792
Vipul Nayyar <nayyar_vipul@yahoo.com>
2013-07-19 11:28:48 +0530

    added fullonsms

---
+++ sms/sendsms.php
@@ -0,0 +1,5 @@
+<?php
+    include('fullonsms-api.php');
+    sendFullonSMS ( '9968371143' , '16537' , '9968371143' , 'Hello World');
+    //sendFullonSMS ( '9000012345' , 'password' , '987654321,9876501234' , 'Hello World');
+?>
