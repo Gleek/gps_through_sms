754ea53dd27f4dfaf07fccc0532d696e2f876506
Vipul Nayyar <nayyar_vipul@yahoo.com>
2013-07-20 07:55:11 +0530

    msgs

--- nav/kookoo/msg.php
+++ nav/kookoo/msg.php
@@ -27,47 +27,48 @@ $con=$dbhandle;
     $text = curl_exec($ch);
     curl_close($ch);
     echo "text is ".$text;
+    return $text;
 }
 
-break_msg("abc");
+$text=break_msg("abc");
+
+if(isset($_REQUEST['event']) && $_REQUEST['event']=="NewSms"){
+	init();
+
+}
+
+
+function init(){
+	$message=$_REQUEST['message'];
+	$time=$_REQUEST['time'];
+	$sql="INSERT INTO `inbound_msgs` (`sender`,`message`, `time`, `flag`) VALUES ('".$_REQUEST['cid']."','".$message."','".$time."',1)";
+	//echo $sql;
+	if(mysql_query($sql, $con)==true) send();
+	else
+		echo "Error in init()";
+}
 
 function send(){
 
-	if(isset($_REQUEST['event']) && $_REQUEST['event']=="NewSms"){
+	if(strlen($_REQUEST['cid'])==10) $sender= $_REQUEST['cid'];
+	else if(strlen($_REQUEST['cid'])>10) $sender= substr($_REQUEST['cid'], -10);
+	else echo "<br/>Error:Length of mobile no less than 10<br/>";
 
+	$d=date ("d");
+	$m=date ("m");
+	$y=date ("Y");
+	$dmt="Current date is ".$d." , month ".$m." & year ".$y.". Here's a random number : ".rand(100,1000);
+	/*$status = sendFullonSMS ( '9968371143' , '16537' , $sender  , $dmt);
 
-		$message=$_REQUEST['message'];
-		$time=$_REQUEST['time'];
-		$sql="INSERT INTO `inbound_msgs` (`sender`,`message`, `time`, `flag`) VALUES ('".$_REQUEST['cid']."','".$message."','".$time."',1)";
-		//echo $sql;
+	if($status[0]['result']==1){
+		$result=mysql_query("SELECT id from inbound_msgs ORDER BY entry_time DESC LIMIT 1", $con);
+		$row = mysql_fetch_array($result);
+		$sql="UPDATE `inbound_msgs` SET `flag`= 2 WHERE `id` = ".$row['id'];
 		mysql_query($sql, $con);
-
-		if(strlen($_REQUEST['cid'])==10) $sender= $_REQUEST['cid'];
-		else if(strlen($_REQUEST['cid'])>10) $sender= substr($_REQUEST['cid'], -10);
-		else echo "<br/>Error:Length of mobile no less than 10<br/>";
-
-		$d=date ("d");
-		$m=date ("m");
-		$y=date ("Y");
-		$dmt="Current date is ".$d." , month ".$m." & year ".$y.". Here's a random number : ".rand(100,1000);
-		$status = sendFullonSMS ( '9968371143' , '16537' , $sender  , $dmt);
-		echo "<pre>".print_r($status)."</pre>";
-		echo "status is ".$status."<br/>";
-
-		if($status[0]['result']==1){
-			$result=mysql_query("SELECT id from inbound_msgs ORDER BY entry_time DESC LIMIT 1", $con);
-			$row = mysql_fetch_array($result);
-			$sql="UPDATE `inbound_msgs` SET `flag`= 2 WHERE `id` = ".$row['id'];
-			mysql_query($sql, $con);
-			echo "trying to put 2";
-		}
-		//mysql_error();
-
-		echo "outside";
-
-
-	}
+	}*/
+	echo "In send text is ".$text;
 }
+
 
 
 //mysql_close($con);
