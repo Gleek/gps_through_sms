fb79f41269cb203abb5e35430fd9a6c7c0309826
=hammad haleem <hammadhaleem@gmail.com>
2013-07-19 23:15:02 -0400

    Added ivrs

--- nav/kookoo/msg.php
+++ nav/kookoo/msg.php
@@ -1,3 +1,4 @@
+
 <?php
 
 include "lib/config.inc.php";
@@ -5,30 +6,66 @@ include('../../sms/fullonsms-api.php');
 //phpinfo();
 $con=$dbhandle;
 
-$message="nav from: jamia millia islamia to: noida";
+function msgparse($message)
+{
+
 $source="";
     $destination="";
-    if(substr($msg,0,3)=="nav")
+    $md=substr($message,0,3);
+    //echo $md;
+    if($md=="nav")
     {
-        $main=explode($message,"from:");
-        $main=explode($main,"to:");
-        $source=url_encode($main[0]);
-        $destination=url_encode($main[1]);
+        
+        $main=preg_split("/(from:|to:)\s*/", $message);
+        $source=urlencode($main[1]);
+        $destination=urlencode($main[2]);
+        $url="http://engineerinme.com/hammad/peerhack/replymsg.php?source=".$source."&destination=".$destination;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        $text = curl_exec($ch);
+        curl_close($ch);
+        //echo $text;
     }
+    else if($md =="loc")
+    {
+        $main=preg_split("/(in:|type:)\s*/", $message);
+        $in=$main[1];
+        $type=$main[2];
 
-    $url="http://engineerinme.com/hammad/peerhack/replymsg.php?source=".$source."&destination=".$destination;
-    echo $url;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-    $text = curl_exec($ch);
-    curl_close($ch);
+        $query="SELECT text FROM market WHERE market='$in' AND keyword ='$type' LIMIT 0,5";
+        $result=mysql_query($query,$con);
+        if (!$result) {
+            $err  = 'Invalid query: ' . mysql_error() . "\n";
+            $err .= 'Whole query: ' . $query;
+            die($err);
+}
+        while($row = mysql_fetch_assoc($result))
+        {
+            $text .= $row['text']." ";
+        }
+
+    }
+    else
+    {
+        $text="Error! Example: 'nav from:lakshmi nagar new delhi india to:lajpat nagar new delhi india' or 'loc in:lajpat type:fabric'";
+    }
     echo $text;
+    
+        $parts = str_split($text, 149);
+        /*echo "<pre>";
+        print_r($parts);
+        echo "</pre>";*/
+        echo "<br>sending ".count($parts)." messages<br>";
+    return $parts
+
+    
+}
 
 
-
-if(isset($_REQUEST['event']) && $_REQUEST['event']=="NewSms" && 1==2){
+if(isset($_REQUEST['event']) && $_REQUEST['event']=="NewSms"){
 
 
 	$message=$_REQUEST['message'];
@@ -40,51 +77,40 @@ if(isset($_REQUEST['event']) && $_REQUEST['event']=="NewSms" && 1==2){
 	if(strlen($_REQUEST['cid'])==10) $sender= $_REQUEST['cid'];
 	else if(strlen($_REQUEST['cid'])>10) $sender= substr($_REQUEST['cid'], -10);
 	else echo "<br/>Error:Length of mobile no less than 10<br/>";
-    $source="";
-    $destination="";
-    if(substr($msg,0,3)=="nav")
-    {
-        $main=explode($message,"from:");
-        $main=explode($main,"to:");
-        $source=url_encode($main[0]);
-        $destination=url_encode($main[1]);
-    }
-    
-    $url="http://engineerinme.com/hammad/peerhack/replymsg.php?source=".$source."&destination=".$destination;
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-    $text = curl_exec($ch);
-    curl_close($ch);
 
 	/*$d=date ("d");
 	$m=date ("m");
 	$y=date ("Y");
 	$dmt="Current date is ".$d." , month ".$m." & year ".$y.". Here's a random number : ".rand(100,1000);*/
-	$status = sendFullonSMS ( '9968371143' , '16537' , $sender  , $dmt);
-	echo "<pre>".print_r($status)."</pre>";
-	echo "status is ".$status."<br/>";
+	
+	
+	$parts=msgparse($message);
+	foreach ($parts as $dmt)
+    {
+	   $status = sendFullonSMS ( '9968371143' , '16537' , $sender  , $dmt);
+	   echo "<pre>".print_r($status)."</pre>";
+	   echo "status is ".$status."<br/>";
 
-	if($status[0]['result']==1){
-		$result=mysql_query("SELECT id from inbound_msgs ORDER BY entry_time DESC LIMIT 1", $con);
-		$row = mysql_fetch_array($result);
-		$sql="UPDATE `inbound_msgs` SET `flag`= 2 WHERE `id` = ".$row['id'];
-		mysql_query($sql, $con);
-		echo "trying to put 2";
+    	if($status[0]['result']==1){
+            $result=mysql_query("SELECT id from inbound_msgs ORDER BY entry_time DESC LIMIT 1", $con);
+            $row = mysql_fetch_array($result);
+	       	$sql="UPDATE `inbound_msgs` SET `flag`= 2 WHERE `id` = ".$row['id'];
+    		mysql_query($sql, $con);
+    		echo "trying to put 2";
 	}
 	//mysql_error();
-
+    }
 	echo "outside";
 
 
 }
 
 
-//mysql_close($con);
+mysql_close($con);
 
 
 
 
 ?>
+
+
--- scrap.py
+++ scrap.py
@@ -3,10 +3,10 @@ from urllib2 import urlopen
 
 import _mysql,MySQLdb
 
-html = urlopen("http://www.justdial.com/Delhi-NCR/Restaurants-%3Cnear%3E-Chandni-Chowk").read()
+html = urlopen("http://www.justdial.com/Delhi-NCR/restaurant-%3Cnear%3E-Lajpat-Nagar").read()
 soup = BeautifulSoup(html, "lxml")
 market="lajpat"
-
+type="restaurant"
 val = soup.findAll("section", "jbc")
 
 add=[]
@@ -33,13 +33,15 @@ for i in name :
     connection = MySQLdb.connect('makemylibrary.co.in', 'thinkdif', 'kgggdkp1992', 'thinkdif_peerhack')
     x = connection.cursor()
     try:
-        q= "INSERT INTO `market`(`market`, `keyword`, `text`) VALUES ('"+str(market)+"','fabric','"+str(i)+str(add[count])+"')"
+        q= "INSERT INTO `market`(`market`, `keyword`, `text`) VALUES ('"+str(market)+"','"+str(type)+"','"+str(i)+str(add[count])+"')"
         print q
         x.execute(q)
         connection.commit()
     except Exception as e :
-        print e
+        print "Error: " + str(e)
         connection.rollback()
     connection.close()
     count =count +1
+
+
 
